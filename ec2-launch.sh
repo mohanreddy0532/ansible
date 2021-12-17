5697d16ca5cfddd730a6a5e50f819bfca4a36a06
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2021-12-17 07:26:20 +0530

    Update ansible/

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -3,4 +3,4 @@
 #1. Create an SPOT instance
 #2. Take that Instance IP & register in DNS
 
-aws ec2 request-spot-instances --instance-count 1 --type "persistent" --launch-specification file://spot.json --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend}]"
+aws ec2 request-spot-instances --instance-count 1 --type "persistent" --launch-specification file://spot.json --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend}]" | jq 
