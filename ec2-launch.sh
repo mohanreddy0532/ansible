1bea38369b4c272b5e75dfb44c12443d0cb63db4
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2021-12-17 07:41:56 +0530

    Update ansible/

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -6,4 +6,8 @@
 TEMP_ID="lt-00c437664b04d92df"
 TEMP_VER=2
 
+## Check if instance is already ther e
+
 aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend}]" "ResourceType=instance,Tags=[{Key=Name,Value=frontend}]" | jq
+
+# Update the DNS record
