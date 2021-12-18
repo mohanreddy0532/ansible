f034cabca8ae67eb6310cfe4a5cd2f5987f0c784
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2021-12-18 06:16:25 +0530

    Update ansible/

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -8,13 +8,15 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
+COMPONENT=$1
+
 
 TEMP_ID="lt-00c437664b04d92df"
 TEMP_VER=2
 
 ## Check if instance is already ther e
 
-aws ec2 describe-instances --filters "Name=tag:Name,Values=frontend" | jq .Reservations[].Instances[].State.Name | sed 's/"//g' | grep -E 'running|stopped' &>/dev/null
+aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].State.Name | sed 's/"//g' | grep -E 'running|stopped' &>/dev/null
 if [ $? -eq -0 ]; then
   echo "Instance is already there"
   exit
@@ -22,6 +24,6 @@ fi
 
 
 
-aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend}]" "ResourceType=instance,Tags=[{Key=Name,Value=frontend}]" | jq
+aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}]" "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
 
 # Update the DNS record
