1c5e4e90bcbd24ce46074a3b3dc510f36be76c90
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2021-12-18 06:14:51 +0530

    Update ansible/

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -8,6 +8,14 @@ TEMP_VER=2
 
 ## Check if instance is already ther e
 
+aws ec2 describe-instances --filters "Name=tag:Name,Values=frontend" | jq .Reservations[].Instances[].State.Name | sed 's/"//g' | grep -E 'running|stopped' &>/dev/null
+if [ $? -eq -0 ]; then
+  echo "Instance is already there"
+  exit
+fi
+
+
+
 aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend}]" "ResourceType=instance,Tags=[{Key=Name,Value=frontend}]" | jq
 
 # Update the DNS record
