0fcf5192907fd68fdb2d7887d9221aef92c71b34
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2021-12-18 06:17:08 +0530

    Update ansible/

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -4,7 +4,7 @@
 #2. Take that Instance IP & register in DNS
 
 if [ -z "$1" ]; then
-  echo "Input is missing"
+  echo -e "\e[1;31mInput is missing\e[0m"
   exit 1
 fi
 
@@ -18,7 +18,7 @@ TEMP_VER=2
 
 aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].State.Name | sed 's/"//g' | grep -E 'running|stopped' &>/dev/null
 if [ $? -eq -0 ]; then
-  echo "Instance is already there"
+  echo -e "\e[1;33mInstance is already there\e[0m"
   exit
 fi
 
